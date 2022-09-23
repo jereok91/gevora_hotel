ecf6b80b7c06ea7e6831a3365faccd10c46b39e2
ELVERRUEDA <elver.s.rueda.b@gmail.com>
2022-09-23 09:57:59 -0500

    conexion servidor

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(auth/login.html)
+    return render_template('auth/index.html')
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
