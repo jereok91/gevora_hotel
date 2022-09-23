42530173bfd7b8c7fe6c7b7b3abfbb16de6183a3
ELVERRUEDA <elver.s.rueda.b@gmail.com>
2022-09-23 10:15:52 -0500

    aparecer imagenes

--- src/app.py
+++ src/app.py
@@ -4,9 +4,9 @@ from config import config
 
 app=Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def index():
-    return render_template('/auth/index.html')
+    return render_template('auth/index.html')
 
 if __name__=='__main__':
     app.config.from_object(config['development'])
